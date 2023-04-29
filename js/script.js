6eec1f6b7901e622068260cfe47631154eb05f76
Martinsgundi <martinsgundi1@gmail.com>
2023-04-29 19:08:45 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const checkRate = ()=> {
+    
+    const height = Number(document.getElementById ('height').value);
+    const weight = Number(document.getElementById ('weight').value);
+    const displayResult = document.getElementById ('show-result');
+
+    const calcBmi = (weight / height**2);
+   
+
+    if (calcBmi < 18.5) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>underweight range</span>) 
+        `
+    };
+
+    if (calcBmi >= 18.5 && calcBmi <=24.9 ) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>healthy weight range</span>) 
+        `
+    };
+
+    if (calcBmi >= 25 && calcBmi <=29.9) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>overweight range</span>) 
+        `
+    };
+
+    if (calcBmi >= 30 && calcBmi <=34.9) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>class I obeysity range</span>) 
+        `
+    };
+
+    if (calcBmi >= 35 && calcBmi <=39.9) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>class II obeysity range</span>) 
+        `
+    };
+
+    if (calcBmi > 40) {
+        displayResult.innerHTML = `
+        Your BMI rate is <b>${calcBmi.toFixed(1)} kg/m<sup>2</sup></b>. </br> 
+        (This falls within the <span>class III obeysity range</span>) 
+        `
+    };
+
+};
+
+
+
+
+
+
